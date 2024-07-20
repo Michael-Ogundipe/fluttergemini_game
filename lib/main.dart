a0722a3b184683571821a2d1584f32449a23585c
Ogundipe Michael <ogundipeibukun51@gmail.com>
2024-07-20 08:18:43 +0100

    initial steps branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
-// import 'package:flame/game.dart';
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(
-//     const GameWidget.controlled(
-//       gameFactory: FlameGame.new,
-//     ),
-//   );
-// }
-
-/// Step 4:
-
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-
-import 'components/game.dart';
 
 void main() {
   runApp(
     const GameWidget.controlled(
-      gameFactory: MyPhysicsGame.new,
+      gameFactory: FlameGame.new,
     ),
   );
 }
+
